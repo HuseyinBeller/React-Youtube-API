895cd158f70e103ae14badd996d6362cd20708de
Hüseyin Beller <77902440+HuseyinBeller@users.noreply.github.com>
2021-07-14 22:44:57 +0200

    Update youtube.js

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'Your-youtube-api-key';
+const KEY = 'Youtube-api-key';
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
